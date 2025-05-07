a6d87ed3db83c3bd86bd4e6afdbb03164f6c26ad
Alexander <alexanderjesusperezmeza@gmail.com>
2025-05-07 16:10:33 -0500

    Agrega widget base para hacer los head los paneles

--- views/widgets/__init__.py
+++ views/widgets/__init__.py
@@ -1,2 +1,3 @@
 from .line_edit_with_button import LineEditWithButton
 from .option_area import OptionArea
+from .panel_head import PanelHead
---
+++ views/widgets/panel_head.py
@@ -0,0 +1,40 @@
+from PySide6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QSizePolicy,
+    QLabel,
+)
+from pathlib import Path
+
+
+class PanelHead(QWidget):
+
+    def __init__(self, title, parent=None):
+        super().__init__(parent)
+
+        self.title = title
+
+        self.setObjectName("PanelHead")
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
+        self.load_style_sheet()
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.main_layout = QVBoxLayout(self)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_layout.setSpacing(20)
+
+        self.title = QLabel(self.title, self)
+        self.title.setObjectName("title")
+        self.main_layout.addWidget(self.title)
+
+        # head widgets setup can be added here
+
+        self.setLayout(self.main_layout)
+
+    def load_style_sheet(self):
+        styles_path = Path(__file__).parent / "panel_head.qss"
+        if styles_path.exists():
+            with open(styles_path, "r") as styles:
+                self.setStyleSheet(styles.read())
