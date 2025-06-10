4afd6cf89a6b5e577f6683be73607bbe559e0341
Alexander <alexanderjesusperezmeza@gmail.com>
2025-06-09 20:04:33 -0500

    Ajusta el layout de PanelHead para usar QHBoxLayout y agrega botones de acción con iconos

--- views/widgets/panel_head.py
+++ views/widgets/panel_head.py
@@ -1,16 +1,18 @@
+from pathlib import Path
 from PySide6.QtWidgets import (
     QWidget,
-    QVBoxLayout,
-    QSizePolicy,
+    QHBoxLayout,
     QLabel,
+    QPushButton,
+    QSizePolicy,
 )
-from pathlib import Path
+from PySide6.QtGui import QIcon
 
 
 class PanelHead(QWidget):
 
     def __init__(self, title, parent=None):
-        super().__init__(parent)
+        super().__init__(parent=parent)
 
         self.title = title
 
@@ -21,20 +23,43 @@ class PanelHead(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-        self.main_layout = QVBoxLayout(self)
+        self.main_layout = QHBoxLayout(self)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setSpacing(20)
+        self.setLayout(self.main_layout)
+
+        # title label
 
         self.title = QLabel(self.title, self)
         self.title.setObjectName("title")
         self.main_layout.addWidget(self.title)
 
-        # head widgets setup can be added here
+        # add stretch
 
-        self.setLayout(self.main_layout)
+        self.main_layout.addStretch()
+
+        # play button
+
+        self.play_button = QPushButton(self)
+        self.play_button.setObjectName("action_button")
+
+        self.play_button_icon = QIcon("assets/play.svg")
+        self.play_button.setIcon(self.play_button_icon)
+
+        self.main_layout.addWidget(self.play_button)
+
+        # star button
+
+        self.star_button = QPushButton(self)
+        self.star_button.setObjectName("action_button")
+
+        self.star_button_icon = QIcon("assets/outlined_star.svg")
+        self.star_button.setIcon(self.star_button_icon)
+
+        self.main_layout.addWidget(self.star_button)
 
     def load_style_sheet(self):
-        styles_path = Path(__file__).parent / "panel_head.qss"
+        styles_path = Path(__file__).with_suffix(".qss")
         if styles_path.exists():
             with open(styles_path, "r") as styles:
                 self.setStyleSheet(styles.read())
