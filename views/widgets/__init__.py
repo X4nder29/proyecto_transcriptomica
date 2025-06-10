5ee5c629b05aa32ee465b870007d099eefc20aac
Alexander <alexanderjesusperezmeza@gmail.com>
2025-06-09 20:11:57 -0500

    Agrega la clase PanelHeadSimple y su hoja de estilo para el encabezado del panel

--- views/widgets/__init__.py
+++ views/widgets/__init__.py
@@ -1,5 +1,6 @@
 from .line_edit_with_button import LineEditWithButton
 from .option_area import OptionArea
 from .panel_head import PanelHead
+from .panel_head_simple import PanelHeadSimple
 from .info_row import InfoRow
 from .work_in_progress_poster_panel import WorkInProgressPosterPanel
