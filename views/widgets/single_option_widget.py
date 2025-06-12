8c426ae2dbcac4e12ace99cfc37c2360733a3db0
Alexander <alexanderjesusperezmeza@gmail.com>
2025-06-11 21:56:43 -0500

    Agrega el widget SingleOptionWidget y su hoja de estilo para mejorar la interfaz de usuario

---
+++ views/widgets/single_option_widget.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+from PySide6.QtWidgets import (
+    QWidget,
+    QHBoxLayout,
+    QStyleOption,
+    QStyle,
+    QLabel,
+    QPushButton,
+)
+from PySide6.QtGui import QPainter, QIcon
+from PySide6.QtCore import Qt
+
+
+class SingleOptionWidget(QWidget):
+
+    def __init__(self, label: str, parent=None):
+        super().__init__(parent)
+
+        self.label = label
+
+        self.setObjectName("SingleOptionWidget")
+        self.setMinimumWidth(300)
+
+        self.load_stylesheet()
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.main_layout = QHBoxLayout(self)
+        self.main_layout.setContentsMargins(10, 10, 10, 10)
+        self.main_layout.setSpacing(10)
+
+        self.checkbox = QPushButton(self)
+        self.checkbox.setObjectName("Checkbox")
+        self.checkbox.setCheckable(True)
+        self.checkbox.setChecked(False)
+        self.checkbox.setIcon(QIcon("assets/checkbox_outlined.svg"))
+        self.main_layout.addWidget(self.checkbox, alignment=Qt.AlignmentFlag.AlignLeft)
+
+        self.name_label = QLabel(self.label, self)
+        self.name_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.main_layout.addWidget(
+            self.name_label, alignment=Qt.AlignmentFlag.AlignVCenter
+        )
+
+        self.main_layout.addStretch()
+
+        self.help_button = QPushButton("?", self)
+        self.help_button.setObjectName("HelpButton")
+        self.help_button.setToolTip("Help")
+        self.main_layout.addWidget(
+            self.help_button, alignment=Qt.AlignmentFlag.AlignRight
+        )
+
+    def load_stylesheet(self):
+        styles_path = Path(__file__).with_suffix(".qss")
+        if styles_path.exists():
+            with open(styles_path, "r") as styles:
+                self.setStyleSheet(styles.read())
+        else:
+            print(f"Stylesheet not found: {styles_path}. Using default styles.")
+
+    def paintEvent(self, event):
+        opt = QStyleOption()
+        opt.initFrom(self)
+        p = QPainter(self)
+        self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, opt, p, self)
+        super().paintEvent(event)
