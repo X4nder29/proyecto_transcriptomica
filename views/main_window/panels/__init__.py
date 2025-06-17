f103928865b4bbf0897b4c02869357428baef027
Alexander <alexanderjesusperezmeza@gmail.com>
2025-06-16 21:27:09 -0500

    Elimina los paneles GraphicsPanel y SettingsPanel del proyecto

--- views/main_window/panels/__init__.py
+++ views/main_window/panels/__init__.py
@@ -1,5 +1,3 @@
 from .home_panel.home_panel import HomePanel
-from .graphics_panel import GraphicsPanel
-from .settings_panel import SettingsPanel
 from .trimmomatic_panel import TrimmomaticPanel
 from .fastqc_panel import FastqcPanel
