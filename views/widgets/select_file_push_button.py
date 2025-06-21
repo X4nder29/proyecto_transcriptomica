5aecb94dfc86d7679d49d408156be734c781642b
Alexander <alexanderjesusperezmeza@gmail.com>
2025-06-20 22:05:44 -0500

    Agrega el widget SelectFilePushButton con su hoja de estilos asociada

---
+++ views/widgets/select_file_push_button.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+from PySide6.QtWidgets import (
+    QWidget,
+    QHBoxLayout,
+    QVBoxLayout,
+    QLabel,
+    QPushButton,
+    QSizePolicy,
+)
+from PySide6.QtGui import QIcon, QFontMetrics
+from PySide6.QtCore import Qt, QFile, QTextStream
+
+
+class SelectFilePushButton(QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.setObjectName("SelectFilePushButton")
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.setMinimumHeight(60)
+
+        self.load_stylesheet()
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.main_layout = QHBoxLayout(self)
+        self.main_layout.setContentsMargins(10, 10, 10, 10)
+        self.main_layout.setSpacing(10)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setLayout(self.main_layout)
+
+        self.icon_label = QLabel(self)
+        self.icon_label.setObjectName("IconLabel")
+        self.icon_label.setPixmap(QIcon(":/assets/upload_file.svg").pixmap(24, 24))
+        self.icon_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.icon_label.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.main_layout.addWidget(self.icon_label)
+
+        self.label_area = QWidget(self)
+        self.label_area.setObjectName("LabelArea")
+        self.main_layout.addWidget(self.label_area)
+
+        self.label_area_layout = QVBoxLayout(self.label_area)
+        self.label_area_layout.setContentsMargins(0, 0, 0, 0)
+        self.label_area_layout.setSpacing(0)
+        self.label_area_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.label_area.setLayout(self.label_area_layout)
+
+        self.primary_label = QLabel(self.label_area)
+        self.primary_label.setObjectName("PrimaryLabel")
+        self.primary_label.setText("Arrastrar y soltar un archivo aquí")
+        self.primary_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.label_area_layout.addWidget(self.primary_label)
+
+        self.secondary_label = QLabel(self.label_area)
+        self.secondary_label.setObjectName("SecondaryLabel")
+        self.secondary_label.setText("O hacer clic para seleccionar un archivo")
+        self.primary_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.label_area_layout.addWidget(self.secondary_label)
+
+    def set_file(self, name: str, path: str):
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+        self.setToolTip(path)
+        self.icon_label.setPixmap(QIcon(":/assets/file.svg").pixmap(24, 24))
+
+        self.primary_label.setText(name)
+        self.primary_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        self.secondary_label.setText(
+            QFontMetrics(self.secondary_label.font()).elidedText(
+                str(path),
+                Qt.TextElideMode.ElideRight,
+                self.secondary_label.width(),
+            )
+        )
+        self.secondary_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+    def clear_file(self):
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setToolTip("")
+        self.icon_label.setPixmap(QIcon(":/assets/upload_file.svg").pixmap(24, 24))
+
+        self.primary_label.setText("Arrastrar y soltar un archivo aquí")
+        self.primary_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.secondary_label.setText("O hacer clic para seleccionar un archivo")
+        self.secondary_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+    def load_stylesheet(self):
+        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+        if qss_file.open(QFile.ReadOnly | QFile.Text):
+            stylesheet = QTextStream(qss_file).readAll() + "\n"
+            self.setStyleSheet(stylesheet)
+            qss_file.close()
+
+    def sizeHint(self):
+        return self.layout().sizeHint()
