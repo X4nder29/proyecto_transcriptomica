198abf354e17c07314c4768e9df9a93708988383
Alexander <alexanderjesusperezmeza@gmail.com>
2025-06-23 16:48:56 -0500

    Agrega la clase CurrentWorkspace y su hoja de estilos asociada

--- views/main_window/panels/home_panel/widgets/__init__.py
+++ views/main_window/panels/home_panel/widgets/__init__.py
@@ -1,2 +1,3 @@
+from .current_workspace import CurrentWorkspace
 from .files_area import FilesArea
 from .file_list_widget import FileListWidget
---
+++ views/main_window/panels/home_panel/widgets/current_workspace.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from PySide6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QStyle,
+    QStyleOption,
+    QSizePolicy,
+)
+from PySide6.QtGui import QPainter
+from PySide6.QtCore import Qt, QFile, QTextStream
+
+
+class CurrentWorkspace(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setObjectName("CurrentWorkspace")
+        self.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+
+        self.load_stylesheet()
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.main_layout = QVBoxLayout(self)
+        self.main_layout.setContentsMargins(20, 20, 20, 20)
+        self.main_layout.setSpacing(10)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter)
+
+        self.workspace_label = QLabel(self)
+        self.workspace_label.setObjectName("WorkspaceLabel")
+        self.main_layout.addWidget(self.workspace_label)
+
+        self.workspace_path = QLabel(self)
+        self.workspace_path.setObjectName("WorkspacePath")
+        self.main_layout.addWidget(self.workspace_path)
+
+    def set_workspace_name(self, name: str):
+        self.workspace_label.setText(name)
+
+    def set_workspace_path(self, path: str):
+        self.workspace_path.setText(path)
+
+    def load_stylesheet(self):
+        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+        if qss_file.open(QFile.ReadOnly | QFile.Text):
+            stylesheet = QTextStream(qss_file).readAll() + "\n"
+            self.setStyleSheet(stylesheet)
+            qss_file.close()
+
+    def paintEvent(self, event):
+        opt = QStyleOption()
+        opt.initFrom(self)
+        p = QPainter(self)
+        self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, opt, p, self)
+        super().paintEvent(event)
