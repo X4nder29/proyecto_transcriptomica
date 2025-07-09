b38a181ea916b73257258c440fd131eaa24a21e6
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-09 01:38:07 -0500

    Elimina el botón de manual de usuario en HomePanelHead

--- views/main_window/panels/home_panel/home_panel_head.py
+++ views/main_window/panels/home_panel/home_panel_head.py
@@ -1,22 +1,8 @@
 from PySide6.QtWidgets import QWidget
-from PySide6.QtCore import Qt
-from views.widgets import PanelHeadBase, ActionButtonWidget
+from views.widgets import PanelHeadBase
 
 
 class HomePanelHead(PanelHeadBase):
 
     def __init__(self, parent: QWidget = None):
         super().__init__("TranscriptoHub", parent)
-
-    def setup_ui(self):
-        super().setup_ui()
-
-        # user manual button
-
-        self.user_manual_button = ActionButtonWidget(
-            icon_path=":/assets/user_manual.svg",
-            tooltip="Abrir manual de usuario",
-            parent=self,
-        )
-        self.user_manual_button.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.main_layout.addWidget(self.user_manual_button)
