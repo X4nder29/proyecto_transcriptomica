2a05ecffad932ea65aee359e29cc942902455d35
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-09 22:56:06 -0500

    Refactoriza la clase HomePanel para mejorar la legibilidad y la estructura del código

--- views/main_window/panels/home_panel/home_panel.py
+++ views/main_window/panels/home_panel/home_panel.py
@@ -2,12 +2,11 @@ from pathlib import Path
 from PySide6.QtWidgets import (
     QWidget,
     QVBoxLayout,
-    QSizePolicy,
-    QPushButton,
     QStyleOption,
     QStyle,
-    QLabel,
+    QSizePolicy,
 )
+from PySide6.QtGui import QPainter
 from PySide6.QtCore import Qt, QFile, QTextStream
 from .home_panel_head import HomePanelHead
 from .home_panel_body import HomePanelBody
@@ -15,24 +14,25 @@ from .home_panel_body import HomePanelBody
 
 class HomePanel(QWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent: QWidget = None):
         super().__init__(parent)
+        self.load_stylesheet()
+        self.setup_ui()
 
+    def setup_ui(self):
         self.setObjectName("HomePanel")
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
-        self.setupUi()
-
-    def setupUi(self):
-        
         self.main_layout = QVBoxLayout(self)
         self.main_layout.setContentsMargins(20, 20, 20, 20)
         self.main_layout.setSpacing(20)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
-        self.panel_head = HomePanelHead(self)
-        self.main_layout.addWidget(self.panel_head, alignment=Qt.AlignmentFlag.AlignTop)
+        self.header = HomePanelHead(self)
+        self.main_layout.addWidget(self.header, alignment=Qt.AlignmentFlag.AlignTop)
 
-        self.panel_body = HomePanelBody(self)
-        self.main_layout.addWidget(self.panel_body)
+        self.content = HomePanelBody(self)
+        self.main_layout.addWidget(self.content)
 
     def load_stylesheet(self):
         qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
@@ -40,3 +40,10 @@ class HomePanel(QWidget):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
             qss_file.close()
+
+    def paintEvent(self, event):
+        opt = QStyleOption()
+        opt.initFrom(self)
+        p = QPainter(self)
+        self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, opt, p, self)
+        super().paintEvent(event)
