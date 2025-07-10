abc0f0de08c739166dabb9897d38b366b3e46eab
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-09 23:45:53 -0500

    Agrega la clase KrakenPanelBody y su hoja de estilo

--- views/main_window/panels/kraken_panel/__init__.py
+++ views/main_window/panels/kraken_panel/__init__.py
@@ -1,1 +1,2 @@
 from .kraken_panel_head import KrakenPanelHead
+from .kraken_panel_body import KrakenPanelBody
---
+++ views/main_window/panels/kraken_panel/kraken_panel_body.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from PySide6.QtWidgets import QWidget, QStyleOption, QStyle, QStackedWidget
+from PySide6.QtGui import QPainter
+from PySide6.QtCore import QFile, QTextStream
+from .pages import FilesPage, OptionsPage
+from views.widgets import GenerationPage
+
+
+class KrakenPanelBody(QStackedWidget):
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent=parent)
+        self.load_stylesheet()
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.setObjectName("KrakenPanelBody")
+
+        self.files_page = FilesPage(self)
+        self.addWidget(self.files_page)
+        
+        self.options_page = OptionsPage(self)
+        self.addWidget(self.options_page)
+
+        self.generation_page_widget = GenerationPage(self)
+        self.addWidget(self.generation_page_widget)
+
+    def load_stylesheet(self):
+        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+        if qss_file.open(QFile.ReadOnly | QFile.Text):
+            stylesheet = QTextStream(qss_file).readAll() + "\n"
+            self.setStyleSheet(stylesheet)
+            qss_file.close()
+
+    def paintEvent(self, _):
+        opt = QStyleOption()
+        opt.initFrom(self)
+        p = QPainter(self)
+        self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, opt, p, self)
