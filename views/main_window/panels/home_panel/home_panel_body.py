48be68bb16ecbf0988e4c7de0c7b97fac4369ff1
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-09 22:53:09 -0500

    Refactoriza HomePanelBody

--- views/main_window/panels/home_panel/home_panel_body.py
+++ views/main_window/panels/home_panel/home_panel_body.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 from PySide6.QtWidgets import (
     QWidget,
-    QHBoxLayout,
+    QGridLayout,
+    QStyleOption,
+    QStyle,
+    QSizePolicy,
 )
+from PySide6.QtGui import QPainter
 from PySide6.QtCore import QFile, QTextStream
+from .widgets import CurrentWorkspace, WorkspaceFilesWidget
 
 
 class HomePanelBody(QWidget):
@@ -12,18 +17,26 @@ class HomePanelBody(QWidget):
         super().__init__(parent)
 
         self.setObjectName("HomePanelBody")
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
         self.load_stylesheet()
 
-        self.setupUi()
+        self.setup_ui()
 
-    def setupUi(self):
-        self.main_layout = QHBoxLayout(self)
+    def setup_ui(self):
+        self.main_layout = QGridLayout(self)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
-        self.main_layout.setSpacing(0)
+        self.main_layout.setSpacing(20)
 
-        self.programs_area = ProgramsArea(self)
-        self.main_layout.addWidget(self.programs_area, alignment=Qt.AlignmentFlag.AlignRight)
+        self.current_workspace = CurrentWorkspace(self)
+        self.main_layout.addWidget(self.current_workspace, 0, 0, 1, 1)
+
+        self.files_area = WorkspaceFilesWidget(self)
+        self.main_layout.addWidget(self.files_area, 1, 0, 2, 2)
+
+        self.main_layout.setColumnStretch(1, 1)
+        self.main_layout.setColumnStretch(2, 1)
+        self.main_layout.setColumnStretch(3, 1)
 
     def load_stylesheet(self):
         qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
@@ -31,3 +44,10 @@ class HomePanelBody(QWidget):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
             qss_file.close()
+
+    def paintEvent(self, event):
+        opt = QStyleOption()
+        opt.initFrom(self)
+        p = QPainter(self)
+        self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, opt, p, self)
+        super().paintEvent(event)
