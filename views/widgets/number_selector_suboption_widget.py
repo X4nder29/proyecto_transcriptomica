9c4f23db9211a81275cc7e04df5f98db596d9779
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-14 22:48:24 -0500

    Refactoriza NumberSelectorSuboptionWidget para mejorar usabilidad

--- views/widgets/number_selector_suboption_widget.py
+++ views/widgets/number_selector_suboption_widget.py
@@ -1,33 +1,34 @@
-from pathlib import Path
 from PySide6.QtWidgets import (
     QWidget,
-    QStyleOption,
-    QStyle,
     QSizePolicy,
     QHBoxLayout,
     QLabel,
 )
-from PySide6.QtGui import QPainter
-from PySide6.QtCore import Qt, QFile, QTextStream
+from PySide6.QtCore import Qt
 from .number_selector import NumberSelector
 from .checkbox_widget import CheckBoxWidget
 
 
 class NumberSelectorSuboptionWidget(QWidget):
-    def __init__(self, title: str, parent: QWidget = None, checkable: bool = False, checked: bool = False):
+    def __init__(
+        self,
+        title: str,
+        parent: QWidget = None,
+        checkable: bool = False,
+        checked: bool = False,
+    ):
         super().__init__(parent)
 
         self.title = title
         self.checkable = checkable
         self.checked = checked
 
-        self.setObjectName("SubOptionWidget")
-        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-
-        self.load_stylesheet()
         self.setup_ui()
 
     def setup_ui(self):
+        self.setObjectName("SubOptionWidget")
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
         self.main_layout = QHBoxLayout(self)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setSpacing(10)
@@ -59,16 +60,8 @@ class NumberSelectorSuboptionWidget(QWidget):
     def toggle_suboption(self, checked: bool):
         self.number_selector.setEnabled(checked)
 
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
-        if qss_file.open(QFile.ReadOnly | QFile.Text):
-            stylesheet = QTextStream(qss_file).readAll() + "\n"
-            self.setStyleSheet(stylesheet)
-            qss_file.close()
+    def value(self):
+        return self.number_selector.value()
 
-    def paintEvent(self, _):
-        opt = QStyleOption()
-        opt.initFrom(self)
-        p = QPainter(self)
-        self.style().drawPrimitive(QStyle.PE_Widget, opt, p, self)
-        super().paintEvent(_)
+    def set_value(self, value: int):
+        self.number_selector.set_value(value)
