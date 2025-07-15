538fafd7d09d8f793750cc683d34b0c25200e4d6
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-15 14:47:17 -0500

    añade sortmernapanel, krakenpanel y settingspanel

--- views/main_window/panels/__init__.py
+++ views/main_window/panels/__init__.py
@@ -1,3 +1,6 @@
 from .home_panel.home_panel import HomePanel
 from .trimmomatic_panel import TrimmomaticPanel
 from .fastqc_panel import FastqcPanel
+from .sortmerna_panel import SortMeRnaPanel
+from .kraken_panel import KrakenPanel
+from .settings_panel import SettingsPanel
