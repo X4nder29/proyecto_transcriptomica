7af9b2c7fcc840bd20f1756f7120ef6f5d20fbd7
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-15 14:46:28 -0500

    organiza las importaciones de panels en un bloque para mejorar la legibilidad

--- views/main_window/__init__.py
+++ views/main_window/__init__.py
@@ -1,4 +1,11 @@
-from .panels import HomePanel, GraphicsPanel, SettingsPanel
 from .main_window import MainWindow
 from .main_window_sidebar import MainWindowSideBar
 from .main_window_content import MainWindowContent
+
+from .panels import (
+    HomePanel,
+    TrimmomaticPanel,
+    FastqcPanel,
+    SortMeRnaPanel,
+    SettingsPanel,
+)
