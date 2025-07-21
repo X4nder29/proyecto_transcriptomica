6162b8ff513aa84f396778e1f791134fc2a8b0b8
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-21 18:40:18 -0500

    Añade las importaciones EmptyWorkspace y CreateWorkspaceDialog en el módulo de vistas

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,3 @@
-from .home_window import HomeWindow
+from .home_window import HomeWindow, EmptyWorkspace, CreateWorkspaceDialog
 from .main_window import MainWindow
 from .widgets import OptionArea, LineEditWithButton
