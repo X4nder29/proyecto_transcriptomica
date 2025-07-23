0d7e6d069b43829957dc4920f519557b1680ae22
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-22 23:16:16 -0500

    Añade la clase InstalledDependencyItem y su hoja de estilos

--- views/main_window/panels/settings_panel/widgets/__init__.py
+++ views/main_window/panels/settings_panel/widgets/__init__.py
@@ -2,3 +2,4 @@ from .installed_widget import InstalledWidget
 from .settings_area import SettingsArea
 from .settings_item import SettingsItem
 from .installed_program_item import InstalledProgramItem
+from .installed_dependency_item import InstalledDependencyItem
---
+++ views/main_window/panels/settings_panel/widgets/installed_dependency_item.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from PySide6.QtWidgets import QLabel, QPushButton
+from PySide6.QtCore import QFile, QTextStream
+from .settings_item import SettingsItem
+
+
+class InstalledDependencyItem(SettingsItem):
+    def __init__(self, icon, title, subtitle, parent=None):
+        super().__init__(icon, title, subtitle, parent)
+
+    def setup_ui(self):
+        super().setup_ui()
+
+        self.checking_indicator = QLabel("Comprobando...")
+        self.checking_indicator.setObjectName("CheckingIndicator")
+        self.action_layout.addWidget(self.checking_indicator)
+
+        self.installed_indicator = QLabel("Instalado")
+        self.installed_indicator.setObjectName("InstalledIndicator")
+        self.installed_indicator.setVisible(False)
+        self.action_layout.addWidget(self.installed_indicator)
+
+        self.install_push_button = QPushButton("Instalar")
+        self.install_push_button.setObjectName("InstallButton")
+        self.install_push_button.setVisible(False)  # Initially hidden
+        self.action_layout.addWidget(self.install_push_button)
+
+    def set_installed(self, installed: bool):
+        self.checking_indicator.setVisible(False)
+        
+        if installed:
+            self.installed_indicator.setVisible(True)
+            self.install_push_button.setVisible(False)
+        else:
+            self.installed_indicator.setVisible(False)
+            self.install_push_button.setVisible(True)
+
+    def load_stylesheet(self):
+        super().load_stylesheet()
+        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+        if qss_file.open(QFile.ReadOnly | QFile.Text):
+            stylesheet = QTextStream(qss_file).readAll() + "\n"
+            self.setStyleSheet(self.styleSheet() + stylesheet)
+            qss_file.close()
