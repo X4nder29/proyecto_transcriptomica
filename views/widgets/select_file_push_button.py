a5d793813a2b070c0530a716c370fe91083bb784
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-25 21:09:41 -0500

    Actualiza el botón de selección de archivo: cambia el ícono y ajusta el texto de las etiquetas

--- views/widgets/select_file_push_button.py
+++ views/widgets/select_file_push_button.py
@@ -35,7 +35,7 @@ class SelectFilePushButton(QPushButton):
 
         self.icon_label = QLabel(self)
         self.icon_label.setObjectName("IconLabel")
-        self.icon_label.setPixmap(QIcon(":/assets/upload_file.svg").pixmap(24, 24))
+        self.icon_label.setPixmap(QIcon(":/assets/file.svg").pixmap(24, 24))
         self.icon_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.icon_label.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         self.main_layout.addWidget(self.icon_label)
@@ -47,20 +47,14 @@ class SelectFilePushButton(QPushButton):
         self.label_area_layout = QVBoxLayout(self.label_area)
         self.label_area_layout.setContentsMargins(0, 0, 0, 0)
         self.label_area_layout.setSpacing(0)
-        self.label_area_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.label_area_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.label_area.setLayout(self.label_area_layout)
 
         self.primary_label = QLabel(self.label_area)
         self.primary_label.setObjectName("PrimaryLabel")
-        self.primary_label.setText("Arrastrar y soltar un archivo aquí")
+        self.primary_label.setText("Clic para seleccionar un archivo")
         self.primary_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         self.label_area_layout.addWidget(self.primary_label)
-
-        self.secondary_label = QLabel(self.label_area)
-        self.secondary_label.setObjectName("SecondaryLabel")
-        self.secondary_label.setText("O hacer clic para seleccionar un archivo")
-        self.secondary_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.label_area_layout.addWidget(self.secondary_label)
 
     def set_file(self, name: str, path: str):
         self.main_layout.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
@@ -76,25 +70,13 @@ class SelectFilePushButton(QPushButton):
         )
         self.primary_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
-        self.secondary_label.setText(
-            QFontMetrics(self.secondary_label.font()).elidedText(
-                str(path),
-                Qt.TextElideMode.ElideRight,
-                self.width() - 60,
-            )
-        )
-        self.secondary_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
-
     def clear_file(self):
         self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setToolTip("")
         self.icon_label.setPixmap(QIcon(":/assets/upload_file.svg").pixmap(24, 24))
 
-        self.primary_label.setText("Arrastrar y soltar un archivo aquí")
+        self.primary_label.setText("Clic para seleccionar un archivo")
         self.primary_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-
-        self.secondary_label.setText("O hacer clic para seleccionar un archivo")
-        self.secondary_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
     def load_stylesheet(self):
         qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
@@ -109,13 +91,6 @@ class SelectFilePushButton(QPushButton):
                 str(self.primary_label.text()),
                 Qt.TextElideMode.ElideRight,
                 self.width(),
-            ),
-        )
-        self.secondary_label.setText(
-            QFontMetrics(self.secondary_label.font()).elidedText(
-                self.secondary_label.text(),
-                Qt.TextElideMode.ElideRight,
-                self.width() - 50,  # Adjusted to account for icon width and padding
             ),
         )
 
