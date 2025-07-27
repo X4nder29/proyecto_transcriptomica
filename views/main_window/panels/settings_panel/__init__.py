3f2988b45412caec7c2b2d7e2c34580b09baad99
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-26 20:06:25 -0500

    Añade importaciones de widgets en el panel de configuración

--- views/main_window/panels/settings_panel/__init__.py
+++ views/main_window/panels/settings_panel/__init__.py
@@ -1,2 +1,10 @@
 from .settings_panel_head import SettingsPanelHead
 from .settings_panel_body import SettingsPanelBody
+
+from .widgets import (
+    SettingsArea,
+    SettingsItem,
+    SimpleActionItem,
+    InstalledProgramItem,
+    InstalledDependencyItem,
+)
