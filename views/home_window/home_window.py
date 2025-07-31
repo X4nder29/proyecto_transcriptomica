6c2c5aa27e55dc0e2ec40383a5bd68a63a72b147
Alexander <alexanderjesusperezmeza@gmail.com>
2025-07-31 18:40:39 -0500

    Elimina importaciones y métodos no utilizados en HomeWindow

--- views/home_window/home_window.py
+++ views/home_window/home_window.py
@@ -1,13 +1,9 @@
-from pathlib import Path
 from PySide6.QtWidgets import (
     QMainWindow,
     QWidget,
-    QVBoxLayout,
-    QStyle,
-    QStyleOption,
+    QVBoxLayout
 )
-from PySide6.QtGui import QIcon, QPainter
-from PySide6.QtCore import QFile, QTextStream
+from PySide6.QtGui import QIcon
 from .home_window_body import HomeWindowBody
 
 
@@ -15,19 +11,14 @@ class HomeWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.setup_ui()
 
+    def setup_ui(self):
+        self.setObjectName("HomeWindow")
         self.setWindowTitle("TranscriptoHub")
         self.setWindowIcon(QIcon(":/assets/icon.svg"))
         self.setMinimumSize(1000, 600)
         self.setAcceptDrops(True)
-
-        self.load_stylesheet()
-
-        self.setup_ui()
-
-    def setup_ui(self):
-
-        self.setObjectName("HomeWindow")
 
         # central widget
 
@@ -44,16 +35,3 @@ class HomeWindow(QMainWindow):
         self.body = HomeWindowBody(self)
         self.body.setObjectName("Body")
         self.central_layout.addWidget(self.body)
-
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
-        if qss_file.open(QFile.ReadOnly | QFile.Text):
-            stylesheet = QTextStream(qss_file).readAll() + "\n"
-            self.setStyleSheet(stylesheet)
-            qss_file.close()
-
-    def paintEvent(self, _):
-        option = QStyleOption()
-        option.initFrom(self)
-        painter = QPainter(self)
-        self.style().drawPrimitive(QStyle.PE_Widget, option, painter, self)
