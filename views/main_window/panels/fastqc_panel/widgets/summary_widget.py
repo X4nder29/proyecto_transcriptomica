3d7a00e2a324e87071120b345651835666a539f3
Alexander <alexanderjesusperezmeza@gmail.com>
2025-08-01 17:53:55 -0500

    Soporte modo oscuro y claro

--- views/main_window/panels/fastqc_panel/widgets/summary_widget.py
+++ views/main_window/panels/fastqc_panel/widgets/summary_widget.py
@@ -8,21 +8,23 @@ from PySide6.QtWidgets import (
     QButtonGroup,
     QPushButton,
 )
+from PySide6.QtGui import QGuiApplication
 from PySide6.QtCore import Qt, QFile, QTextStream
+from utils import tint_icon
 
 
 class SummaryListWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent: QWidget = None):
         super().__init__(parent)
+        self.load_stylesheet(QGuiApplication.styleHints().colorScheme())
+        self.setup_ui()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
 
+    def setup_ui(self):
         self.setObjectName("SummaryListWidget")
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.setMinimumWidth(300)
 
-        self.load_stylesheet()
-        self.setup_ui()
-
-    def setup_ui(self):
         self.main_layout = QVBoxLayout(self)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setSpacing(10)
@@ -159,9 +161,15 @@ class SummaryListWidget(QWidget):
 
         self.main_layout.addStretch()
 
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
         if qss_file.open(QFile.ReadOnly | QFile.Text):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
+            self.update()
             qss_file.close()
