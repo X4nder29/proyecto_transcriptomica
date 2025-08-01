f2f309fa203eaa96b4a87cd21856023f3c5cbcd2
Alexander <alexanderjesusperezmeza@gmail.com>
2025-08-01 11:13:47 -0500

    Soporte para modo oscuro y claro

--- views/widgets/action_button_widget.py
+++ views/widgets/action_button_widget.py
@@ -1,25 +1,34 @@
 from pathlib import Path
-from PySide6.QtWidgets import QPushButton
-from PySide6.QtGui import QIcon
-from PySide6.QtCore import QFile, QTextStream
+from PySide6.QtWidgets import QWidget, QPushButton
+from PySide6.QtGui import QGuiApplication, QIcon
+from PySide6.QtCore import Qt, QFile, QTextStream
 
 
 class ActionButtonWidget(QPushButton):
-    def __init__(self, icon_path: str, tooltip: str, parent=None):
+    def __init__(self, icon_path: str, tooltip: str, parent: QWidget = None):
         super().__init__(parent=parent)
         self.icon_path = icon_path
         self.tooltip = tooltip
         self.setup_ui()
-        self.load_stylesheet()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
+        QGuiApplication.styleHints().colorSchemeChanged.emit(
+            QGuiApplication.styleHints().colorScheme()
+        )
 
     def setup_ui(self):
         self.setObjectName("ActionButtonWidget")
         self.setToolTip(self.tooltip)
         self.setIcon(QIcon(self.icon_path))
 
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
         if qss_file.open(QFile.ReadOnly | QFile.Text):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
+            self.update()
             qss_file.close()
