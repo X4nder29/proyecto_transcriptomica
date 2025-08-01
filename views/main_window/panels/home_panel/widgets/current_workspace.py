42aada6204a0bda8d8c067c210be06fb67a9e2f5
Alexander <alexanderjesusperezmeza@gmail.com>
2025-08-01 11:50:02 -0500

    Soporte modo oscuro y claro

--- views/main_window/panels/home_panel/widgets/current_workspace.py
+++ views/main_window/panels/home_panel/widgets/current_workspace.py
@@ -7,22 +7,23 @@ from PySide6.QtWidgets import (
     QStyleOption,
     QSizePolicy,
 )
-from PySide6.QtGui import QPainter
+from PySide6.QtGui import QGuiApplication, QPainter
 from PySide6.QtCore import Qt, QFile, QTextStream
 
 
 class CurrentWorkspace(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setup_ui()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
+        QGuiApplication.styleHints().colorSchemeChanged.emit(
+            QGuiApplication.styleHints().colorScheme()
+        )
 
+    def setup_ui(self):
         self.setObjectName("CurrentWorkspace")
         self.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
 
-        self.load_stylesheet()
-
-        self.setup_ui()
-
-    def setup_ui(self):
         self.main_layout = QVBoxLayout(self)
         self.main_layout.setContentsMargins(20, 20, 20, 20)
         self.main_layout.setSpacing(10)
@@ -42,11 +43,17 @@ class CurrentWorkspace(QWidget):
     def set_workspace_path(self, path: str):
         self.workspace_path.setText(path)
 
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
         if qss_file.open(QFile.ReadOnly | QFile.Text):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
+            self.update()
             qss_file.close()
 
     def paintEvent(self, event):
