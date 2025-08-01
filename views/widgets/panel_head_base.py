8c617b71eea7bebf4658e97b9628da365517adb2
Alexander <alexanderjesusperezmeza@gmail.com>
2025-08-01 11:30:22 -0500

    Soporte modo oscuro y claro

--- views/widgets/panel_head_base.py
+++ views/widgets/panel_head_base.py
@@ -4,7 +4,8 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QLabel,
 )
-from PySide6.QtCore import QFile, QTextStream
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import Qt, QFile, QTextStream
 
 
 class PanelHeadBase(QWidget):
@@ -12,8 +13,11 @@ class PanelHeadBase(QWidget):
     def __init__(self, title: str, parent: QWidget = None):
         super().__init__(parent=parent)
         self.title = title
-        self.load_stylesheet()
         self.setup_ui()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
+        QGuiApplication.styleHints().colorSchemeChanged.emit(
+            QGuiApplication.styleHints().colorScheme()
+        )
 
     def setup_ui(self):
         self.setObjectName("PanelHeadBase")
@@ -31,9 +35,15 @@ class PanelHeadBase(QWidget):
 
         self.main_layout.addStretch()
 
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
         if qss_file.open(QFile.ReadOnly | QFile.Text):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
+            self.update()
             qss_file.close()
