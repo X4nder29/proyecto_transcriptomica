5f32d450ba9abdc6ee260ee626d1ed832af0b63f
Alexander <alexanderjesusperezmeza@gmail.com>
2025-08-01 20:23:37 -0500

    Soporte modo oscuro y claro

--- views/main_window/panels/fastqc_panel/widgets/unselected_report_widget.py
+++ views/main_window/panels/fastqc_panel/widgets/unselected_report_widget.py
@@ -1,20 +1,20 @@
 from pathlib import Path
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QSizePolicy, QStyleOption, QStyle, QLabel
-from PySide6.QtGui import QPainter
+from PySide6.QtGui import QGuiApplication, QPainter
 from PySide6.QtCore import Qt, QFile, QTextStream
 
 
 class UnselectedReportWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent: QWidget = None):
         super().__init__(parent)
+        self.load_stylesheet(QGuiApplication.styleHints().colorScheme())
+        self.setup_ui()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
 
+    def setup_ui(self):
         self.setObjectName("UnselectedReportWidget")
         self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
-        self.load_stylesheet()
-        self.setup_ui()
-
-    def setup_ui(self):
         self.main_layout = QVBoxLayout(self)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setSpacing(0)
@@ -33,14 +33,20 @@ class UnselectedReportWidget(QWidget):
 
         self.label = QLabel(self)
         self.label.setObjectName("UnselectedReportLabel")
-        self.label.setText("Select a report to view its details.")
+        self.label.setText("Seleccione un informe para ver sus detalles")
         self.container_layout.addWidget(self.label)
 
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
         if qss_file.open(QFile.ReadOnly | QFile.Text):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
+            self.update()
             qss_file.close()
 
     def paintEvent(self, _):
