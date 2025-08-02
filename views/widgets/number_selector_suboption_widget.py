822e6dd58d46bebadaa311d0cc0379b0945a9602
Alexander <alexanderjesusperezmeza@gmail.com>
2025-08-02 11:36:27 -0500

    Soporte modo oscuro y claro

--- views/widgets/number_selector_suboption_widget.py
+++ views/widgets/number_selector_suboption_widget.py
@@ -1,10 +1,12 @@
+from pathlib import Path
 from PySide6.QtWidgets import (
     QWidget,
     QSizePolicy,
     QHBoxLayout,
     QLabel,
 )
-from PySide6.QtCore import Qt
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import Qt, QFile, QTextStream
 from .number_selector import NumberSelector
 from .checkbox_widget import CheckBoxWidget
 
@@ -23,7 +25,9 @@ class NumberSelectorSuboptionWidget(QWidget):
         self.checkable = checkable
         self.checked = checked
 
+        self.load_stylesheet(QGuiApplication.styleHints().colorScheme())
         self.setup_ui()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
 
     def setup_ui(self):
         self.setObjectName("SubOptionWidget")
@@ -74,7 +78,20 @@ class NumberSelectorSuboptionWidget(QWidget):
 
     def is_checked(self):
         return self.check_box_widget.isChecked() if self.checkable else True
-    
+
     def set_checked(self, checked: bool):
         if self.checkable:
             self.check_box_widget.setChecked(checked)
+
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
+        if qss_file.open(QFile.ReadOnly | QFile.Text):
+            stylesheet = QTextStream(qss_file).readAll() + "\n"
+            self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
+            self.update()
+            qss_file.close()
