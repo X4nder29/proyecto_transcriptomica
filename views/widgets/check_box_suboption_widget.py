efd0c2b37e9a24e1b2407aac3de077420817f6a9
Alexander <alexanderjesusperezmeza@gmail.com>
2025-08-02 19:11:55 -0500

    Soporte modo oscuro y claro

--- views/widgets/check_box_suboption_widget.py
+++ views/widgets/check_box_suboption_widget.py
@@ -1,9 +1,11 @@
+from pathlib import Path
 from PySide6.QtWidgets import (
     QWidget,
     QHBoxLayout,
     QLabel,
 )
-from PySide6.QtCore import Qt
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import Qt, QFile, QTextStream
 from .checkbox_widget import CheckBoxWidget
 
 
@@ -12,11 +14,14 @@ class CheckBoxSuboptionWidget(QWidget):
         super().__init__(parent)
         self.title = title
         self.checked = checked
-        self.setObjectName("CheckBoxSuboptionWidget")
-        self.setMinimumWidth(300)
+        self.load_stylesheet(QGuiApplication.styleHints().colorScheme())
         self.setup_ui()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
 
     def setup_ui(self):
+        self.setObjectName("CheckBoxSuboptionWidget")
+        self.setMinimumWidth(300)
+
         self.main_layout = QHBoxLayout(self)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setSpacing(10)
@@ -36,7 +41,20 @@ class CheckBoxSuboptionWidget(QWidget):
     def checked(self) -> bool:
         """Returns the checked state of the checkbox."""
         return self.checkbox.isChecked()
-    
+
     def set_checked(self, checked: bool):
         """Sets the checked state of the checkbox."""
         self.checkbox.setChecked(checked)
+
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
+        if qss_file.open(QFile.ReadOnly | QFile.Text):
+            stylesheet = QTextStream(qss_file).readAll() + "\n"
+            self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
+            self.update()
+            qss_file.close()
