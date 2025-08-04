6f52f8d795246ff052900c4bb97351be13a91fe5
Alexander <alexanderjesusperezmeza@gmail.com>
2025-08-03 23:24:44 -0500

    Soporte modo oscuro y claro

--- views/support_window/support_window_content.py
+++ views/support_window/support_window_content.py
@@ -2,22 +2,20 @@ from pathlib import Path
 from PySide6.QtWidgets import (
     QWidget,
     QVBoxLayout,
-    QHBoxLayout,
     QStyleOption,
     QStyle,
-    QLineEdit,
-    QPushButton,
     QTextBrowser,
 )
-from PySide6.QtGui import QPainter
-from PySide6.QtCore import QFile, QTextStream
+from PySide6.QtGui import QGuiApplication, QPainter
+from PySide6.QtCore import Qt, QFile, QTextStream
 
 
 class SupportWindowContent(QWidget):
     def __init__(self, parent: QWidget = None):
         super().__init__(parent)
-        self.load_stylesheet()
+        self.load_stylesheet(QGuiApplication.styleHints().colorScheme())
         self.setup_ui()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
 
     def setup_ui(self):
         self.main_layout = QVBoxLayout(self)
@@ -30,11 +28,17 @@ class SupportWindowContent(QWidget):
         self.viewer.setOpenExternalLinks(True)
         self.main_layout.addWidget(self.viewer)
 
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
         if qss_file.open(QFile.ReadOnly | QFile.Text):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
+            self.update()
             qss_file.close()
 
     def paintEvent(self, _):
--- views/support_window/support_window_sidebar.py
+++ views/support_window/support_window_sidebar.py
@@ -1,22 +1,29 @@
 from pathlib import Path
 from PySide6.QtWidgets import QWidget, QListWidget
+from PySide6.QtGui import QGuiApplication
 from PySide6.QtCore import Qt, QFile, QTextStream
 
 
 class SupportWindowSidebar(QListWidget):
     def __init__(self, parent: QWidget = None):
         super().__init__(parent=parent)
-        self.load_stylesheet()
+        self.load_stylesheet(QGuiApplication.styleHints().colorScheme())
         self.setup_ui()
+        QGuiApplication.styleHints().colorSchemeChanged.connect(self.load_stylesheet)
 
     def setup_ui(self):
         self.setObjectName("SupportWindowSidebar")
         self.setSpacing(5)
         self.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 
-    def load_stylesheet(self):
-        qss_file = QFile(f":/styles/{Path(__file__).stem}.qss")
+    def load_stylesheet(self, scheme: Qt.ColorScheme):
+        qss_file = QFile(
+            f":/styles/{Path(__file__).stem}_{"dark" if scheme == Qt.ColorScheme.Dark else "light"}.qss"
+        )
         if qss_file.open(QFile.ReadOnly | QFile.Text):
             stylesheet = QTextStream(qss_file).readAll() + "\n"
             self.setStyleSheet(stylesheet)
+            style = self.style()
+            style.unpolish(self)
+            style.polish(self)
             qss_file.close()
